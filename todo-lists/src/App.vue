8698e6aaf236e23066b0e1adf35a36293370254a
Zeynep BÖLÜKBAŞI <93818946+bookwormzn@users.noreply.github.com>
2024-08-06 23:32:44 +0300

    Add files via upload

---
+++ todo-lists/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <todo-list></todo-list>
+  </div>
+</template>
+
+<script>
+import TodoList from './components/TodoList.vue';
+
+export default {
+  name: 'App',
+  components: {
+    TodoList
+  }
+}
+</script>
+
+<style>
+body {
+  font-family: 'Arial', sans-serif;
+  background-color: #f0f8ff;
+  color: #333;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+#app {
+  background: #fff;
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  width: 400px;
+  max-width: 100%;
+}
+</style>
